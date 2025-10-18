6fedd1e95aa22d1135c19ba59becf5ca8497599b
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-10-18 23:03:42 +0530

    making the auth  pages

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export  default function Page(){
+    return(
+        <div className="max-w-7xl min-h-screen  mx-auto w-full bg-red-500 flex justify-center items-center">
+            Hello
+        </div>
+    )
+}
